b53461069890db89faff318631f40f85cf06a215
Ryan Gruss <gruss.ryan@gmail.com>
2018-12-29 17:19:34 -0800

    Added thermostat control smartapp

---
+++ smartapps/thermostatcontrol/parent/smart-thermostat-control.src/smart-thermostat-control.groovy
@@ -0,0 +1,39 @@
+definition(
+    name: "Smart Thermostat Control",
+    namespace: "thermostatcontrol/parent",
+    author: "Ryan",
+    description: "Automate multiple thermostats (this is the parent).",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+    // The parent app preferences are pretty simple: just use the app input for the child app.
+    page(name: "mainPage", title: "Simple Thermostat Control", install: true, uninstall: true,submitOnChange: true) {
+        section {
+            app(name: "simpleAutomation", appName: "Simple Thermostat Control", namespace: "thermostatcontrol/automations", title: "Create New Automation", multiple: true)
+            }
+    }
+}
+
+def installed() {
+    log.debug "Installed with settings: ${settings}"
+    initialize()
+}
+
+def updated() {
+    log.debug "Updated with settings: ${settings}"
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+    // nothing needed here, since the child apps will handle preferences/subscriptions
+    // this just logs some messages for demo/information purposes
+    log.debug "there are ${childApps.size()} child smartapps"
+    childApps.each {child ->
+        log.debug "child app: ${child.label}"
+    }
+}
